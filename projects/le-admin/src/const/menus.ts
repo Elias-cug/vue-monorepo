1b3fcecdd4c7c46decf19ceee5084bb3e3c812c3
elias lee <liyisheng0113@163.com>
2026-01-27 09:41:13 +0800

    feat: 菜单获取方式定义

---
+++ projects/le-admin/src/const/menus.ts
@@ -0,0 +1,13 @@
+export const menuList = [
+  {
+    key: 'profile',
+    title: '个人中心',
+    pinned: true,
+  },
+  {
+    key: 'user-management',
+    title: '用户管理',
+  },
+];
+
+export default { data: menuList };
