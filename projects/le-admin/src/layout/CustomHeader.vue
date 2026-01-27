9b9e2ee906a963b9f7b827922e8f52f796a30082
elias lee <liyisheng0113@163.com>
2026-01-27 15:10:58 +0800

    feat: 表头修改

--- projects/le-admin/src/layout/CustomHeader.vue
+++ projects/le-admin/src/layout/CustomHeader.vue
@@ -1,20 +1,9 @@
 <template>
-  <CommonHeader :show-opts="['user', 'theme']">
-    <template #header-center>
-      <div class="w-40%">
-        <NInput round placeholder="请输入搜索内容">
-          <template #suffix>
-            <NIcon :component="SearchOutline" />
-          </template>
-        </NInput>
-      </div>
-    </template>
-  </CommonHeader>
+  <CommonHeader :show-opts="['user', 'theme']"></CommonHeader>
 </template>
 
 <script setup lang="ts">
 import { CommonHeader } from '@base/layout';
-import { SearchOutline } from '@vicons/ionicons5';
 defineOptions({
   name: 'CustomHeader',
 });
